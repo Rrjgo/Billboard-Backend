c2a0adbb741581c4f8a3eef951e4bf56c4b10e1d
howard <hjia095@aucklanduni.ac.nz>
2021-07-08 23:21:35 +1200

    fixed cors error

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const messageRoutes = require('./routes/message')
 
 const app = express();
 const port = 8000;
 
+
+//connect to mongodb
 mongoose.connect(process.env.MONGOOSE_URL || "" ,{
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -19,16 +22,15 @@ mongoose.connection.once('open',() => {})
 
 app.use(express.json({extended: true}));
 app.use(express.urlencoded({extended: true}));
+app.use(cors())
 
-
-
+//defult page
 app.get('/', (req, res) => {
   res.send('Billboard-backend')
 });
 
+//send and post message page
 app.use("/msg",messageRoutes)
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
