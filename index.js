fdbd1b29fcacdd63e0041166317a76fd4697a672
Rrjgo <howardrrjgo@gmail.com>
2021-07-14 14:05:07 +1200

    changed port setting

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const messageRoutes = require('./routes/message')
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 
 //connect to mongodb
